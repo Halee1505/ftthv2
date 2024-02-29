59ca1d9246a347982b760f3e3235c16ad2cc3974
Halee1505 <linh.nguyen1505@hcmut.edu.vn>
2024-02-29 14:42:28 +0700

    add sitemap

---
+++ src/app/api/sitemap/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+export async function GET() {
+  //export  sitemap.xml from public folder
+  return await fetch("https://k6cysl7xua9ovkxs.public.blob.vercel-storage.com/ftth-sitemap.xml").then((res) => {
+    return new Response(res.body, {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    });
+  });
+}
